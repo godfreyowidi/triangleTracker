c0a321f4f7c1ad09e62f6781231fcc613a57cbb7
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-04 01:15:26 +0300

    add forms display for the 3 length sides.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+const length1 = parseInt(prompt("Enter length1:"));
+const length2 = parseInt(promp("Enter length2:"));
+const length3 = parseInt(prompt("Enter length3:"));
+
+/*function equilateral() {
+  if (length1 === length2 && length2 === length3) {
+  return true;
+} else {
+  return false;
+}
+
+function isosceles() {
+  if (length1 === length2 && length1 === length3) {
+  return true;
+} else {
+  return false;
+}
+
+function scalene() {
+  if (length1 !== length2 && length1 !== length3) {
+  return true;
+} else {
+  return false;
+}
+
+function notTriangle() {
+  if (length1 + length2 === length3) {
+    return true;
+  } else if (length1 + length3 === length3) {
+    return true;
+  } else if (length2 + length3 === length1) {
+    return true;
+  } else {
+    return false;
+  }
+}*/
